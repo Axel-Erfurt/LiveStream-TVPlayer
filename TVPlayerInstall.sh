6c1dfb1d80ebb798b4235305dbb7c32ad5c7ef09
Axel Schneider <axel99092@t-online.de>
2020-01-05 19:20:45 +0100

    Update TVPlayerInstall.sh

--- TVPlayerInstall.sh
+++ TVPlayerInstall.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd ~/.local/share/
-echo "estelle Ordner TVPlayer"
 echo "TVPlayer herunterladen ..."
 wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer/archive/master.zip
 echo "TVPlayer entpacken"
@@ -8,6 +7,7 @@ unzip -o master.zip
 echo "zip Datei wird entfernt"
 rm master.zip
 chmod +x ~/.local/share/LiveStream-TVPlayer-master/mediaterm
+echo "Senderliste aktualisieren"
 python3 ~/.local/share/LiveStream-TVPlayer-master/Sender_aktualisieren.py ~/.local/share/LiveStream-TVPlayer-master
 echo "TVPlayer wird gestartet ..."
 python3 ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.py
