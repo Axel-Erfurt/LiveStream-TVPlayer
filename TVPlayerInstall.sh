4b604565884577e6ddee325f200122fad1f37337
Axel Schneider <axel99092@t-online.de>
2020-01-05 19:04:13 +0100

    Add files via upload

---
+++ TVPlayerInstall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd ~/.local/share/
+echo "estelle Ordner TVPlayer"
+echo "TVPlayer herunterladen ..."
+wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer/archive/master.zip
+echo "TVPlayer entpacken"
+unzip -o master.zip
+echo "zip Datei wird entfernt"
+rm master.zip
+rm ~/.local/share/LiveStream-TVPlayer-master/tv_listen/*
+chmod +x ~/.local/share/LiveStream-TVPlayer-master/mediaterm
+python3 ~/.local/share/LiveStream-TVPlayer-master/Sender_aktualisieren.py ~/.local/share/LiveStream-TVPlayer-master
+echo "TVPlayer wird gestartet ..."
+python3 ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.py
+exit
+
