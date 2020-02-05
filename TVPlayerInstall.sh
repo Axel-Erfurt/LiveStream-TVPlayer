558cd559bec9074301c30afde5d92c4b76dc5a23
Axel Schneider <axel99092@t-online.de>
2020-02-05 21:14:31 +0100

    Update TVPlayerInstall.sh
    new Installer keeping old tv list

--- TVPlayerInstall.sh
+++ TVPlayerInstall.sh
@@ -1,11 +1,49 @@
 #!/bin/sh
+filename=$HOME/.local/share/LiveStream-TVPlayer-master/tv_listen
+if [ -d "$filename" ]
+then
+    echo "$filename found, copying to /tmp"
+    cp -rf $filename /tmp
+else
+    echo "$filename not found"
+fi
+sharedapps=$HOME/.local/share/applications/
+if [ -d "$sharedapps" ]
+ then
+    echo "$sharedapps found"
+else
+    echo "$sharedapps not found"
+    mkdir $sharedapps
+fi
+echo "removing TVPlayer2"
+rm -rf ~/.local/share/LiveStream-TVPlayer-master
 cd ~/.local/share/
 echo "downloading TVPlayer2 ..."
-wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer/archive/master.zip
-echo "unzip TVPlayer"
+wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer-Deutsch/archive/master.zip
+echo "unzip TVPlayer2"
 unzip -o master.zip
+sleep 1
 echo "remove zip file"
 rm master.zip
-cp ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.desktop ~/.local/share/applications
-echo "starting TVPlayer2 ..."
+mv ~/.local/share/LiveStream-TVPlayer-Deutsch-master ~/.local/share/LiveStream-TVPlayer-master
+rf=/tmp/tv_listen
+if [ -d "$rf" ]
+then
+    echo "restore tv_listen"
+    cp $rf -rf $HOME/.local/share/LiveStream-TVPlayer-master
+else
+    echo "$rf not found"
+fi
+desktopfile=$HOME/.local/share/applications/TVPlayer2.desktop
+if [ -e "$desktopfile" ]
+then
+    echo "$desktopfile already exists"
+else
+    echo "$desktopfile not found"
+    cp $HOME/.local/share/LiveStream-TVPlayer-master/TVPlayer2.desktop $HOME/.local/share/applications
+fi
+rm ~/Downloads/TVPlayerInstall.sh
+echo "check Channels ... "
+python3 ~/.local/share/LiveStream-TVPlayer-master/query_mv.py ~/.local/share/LiveStream-TVPlayer-master/
+echo "starting TVPlayer2 ... "
 python3 ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.py
