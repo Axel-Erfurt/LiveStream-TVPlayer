32e029bef4f3b5fc5c31310a130a627b6ccfb11b
Axel Schneider <axel99092@t-online.de>
2020-02-05 21:27:30 +0100

    Update TVPlayerInstall.sh

--- TVPlayerInstall.sh
+++ TVPlayerInstall.sh
@@ -19,12 +19,13 @@ echo "removing TVPlayer2"
 rm -rf ~/.local/share/LiveStream-TVPlayer-master
 cd ~/.local/share/
 echo "downloading TVPlayer2 ..."
-wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer-Deutsch/archive/master.zip
+wget https://github.com/Axel-Erfurt/LiveStream-TVPlayer/archive/master.zip
 echo "unzip TVPlayer2"
 unzip -o master.zip
 sleep 1
 echo "remove zip file"
 rm master.zip
+#mv ~/.local/share/LiveStream-TVPlayer-master ~/.local/share/LiveStream-TVPlayer-master
 rf=/tmp/tv_listen
 if [ -d "$rf" ]
 then
@@ -42,5 +43,7 @@ else
     cp $HOME/.local/share/LiveStream-TVPlayer-master/TVPlayer2.desktop $HOME/.local/share/applications
 fi
 rm ~/Downloads/TVPlayerInstall.sh
+echo "check Channels ... "
+python3 ~/.local/share/LiveStream-TVPlayer-master/query_mv.py ~/.local/share/LiveStream-TVPlayer-master/
 echo "starting TVPlayer2 ... "
 python3 ~/.local/share/LiveStream-TVPlayer-master/TVPlayer2.py
